36e188724ee385ed49bc18c06891bc1f2843b0b9
jpanacleto2 <jpanacleto5@gmail.com>
2023-07-13 17:26:53 -0300

    script retornando dados limpos

--- model/script.py
+++ model/script.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import os
 import requests
+from bs4 import BeautifulSoup
 
+# Baixar as propostas esta dando erro de certificado ssl
 def baixa_propostas(url):
     # Obter o nome do arquivo a partir do link
     file_name = os.path.basename(url)
@@ -22,7 +24,30 @@ def baixa_propostas(url):
     else:
         print("Falha ao fazer o download do arquivo.")
 
-# Exemplo de uso da função
-url = "https://brasilparticipativo.presidencia.gov.br/open-data/download"
-baixa_propostas(url)
+
+
+propostas = pd.read_csv("model/brasilparticipativo.presidencia.gov.br-open-data-proposals.csv", delimiter=";")
+
+#seleciona as colunas que precisa
+propostas = pd.DataFrame(propostas, columns=['category/name/pt-BR','title/pt-BR','body/pt-BR'])
+
+#retira as linhas que estao vazias
+proposta = propostas.dropna()
+
+# Coluna somente com as propostas
+coluna_html = propostas['body/pt-BR']
+
+# retira as tags html da coluna de propostas do csv
+texto_extraido = []
+for elemento_html in coluna_html:
+    soup = BeautifulSoup(elemento_html, 'html.parser')
+    texto = soup.get_text()
+    texto_extraido.append(texto)
+
+propostas['body/pt-BR'] = texto_extraido  # Sobrescreve a coluna com o texto extraído
+
+#Retorna o novo csv limpo
+
+propostas.to_csv('model/propostas.csv', index=False)
+
 
